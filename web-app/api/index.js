7799dc7ac9a98b04a7778ff997436744f13cc44b
wqinCS <k2011765@students.katyisd.org>
2022-03-03 08:05:21 -0600

    add mongoose

--- web-app/api/index.js
+++ web-app/api/index.js
@@ -1,6 +1,7 @@
-const app = require('express')();
-const { v4 } = require('uuid');
 
+const app = require('express')();
+//const { v4 } = require('uuid');
+/*
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
@@ -12,5 +13,30 @@ app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
-
+*/
 module.exports = app;
+
+//
+
+const mongoose = require("mongoose");
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+};
+
+const CONNECTION_STRING = "MONGODB_CONNECTION_STRING";
+
+module.exports = () => {
+  mongoose.connect(CONNECTION_STRING, options);
+
+  const connection = mongoose.connection;
+  connection.once("open", () => {
+    console.log("MongoDB database connection established successfully.");
+  });
+
+  mongoose.connection.on("error", err => {
+    console.log(err);
+  });
+};
