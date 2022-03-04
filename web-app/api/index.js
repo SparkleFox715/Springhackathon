713371c8de1d3305c58389cfa95124f4c69fc82a
wqinCS <k2011765@students.katyisd.org>
2022-03-04 08:05:40 -0600

    mongoose

--- web-app/api/index.js
+++ web-app/api/index.js
@@ -1,42 +1,38 @@
 
+const DEV_PORT = 3000;
+
 const app = require('express')();
-//const { v4 } = require('uuid');
-/*
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-*/
-module.exports = app;
-
-//
-
 const mongoose = require("mongoose");
 
-const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-};
+function doDuringConnectedToMongoDB(innerFunct) {
 
-const CONNECTION_STRING = "MONGODB_CONNECTION_STRING";
+  const options = {
+    useNewUrlParser     : true,
+  //useCreateIndex      : true,
+    useUnifiedTopology  : true,
+  };
+  const CONNECTION_URI = process.env.MONGODB_URI || `mongodb://localhost:${DEV_PORT}/dev`;
 
-module.exports = () => {
-  mongoose.connect(CONNECTION_STRING, options);
+  return (...args) => {
+  
+    mongoose.connect(CONNECTION_URI, options);
 
-  const connection = mongoose.connection;
-  connection.once("open", () => {
-    console.log("MongoDB database connection established successfully.");
-  });
+    const connection = mongoose.connection;
 
-  mongoose.connection.on("error", err => {
-    console.log(err);
-  });
-};
+    connection.once("open", () => {
+      console.log("MongoDB database connection established successfully.");
+    });
+
+    mongoose.connection.on("error", err => {
+      console.log(err);
+    });
+
+    return innerFunct(connection, ...args);
+  }
+}
+
+app.get('/api/question', doDuringConnectedToMongoDB((connection, req, res) => {
+  res.status(200).end(`GET a question ${connection}`);
+}));
+
+module.exports = app;
