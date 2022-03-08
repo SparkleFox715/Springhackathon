4c7e00d9188f1e47f90bf3dd867bb8d455aa1c46
SparkleFox715 <jupiter.am705@gmail.com>
2022-03-08 08:10:15 -0600

    react changes

--- web-app/front-end/src/App.js
+++ web-app/front-end/src/App.js
@@ -3,23 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="loginpage">
+        
+      </div>
   );
 }
 
+
 export default App;
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn Cool things
+    //     </a>
+    //   </header>
+    // </div>
